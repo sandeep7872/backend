64bd165cda62d52c2367edc0164c5799ad57857c
sandeep7872 <93874397+sandeep7872@users.noreply.github.com>
2025-07-22 15:41:50 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,7 @@
-
 const express = require("express");
 const router = express.Router();
-const Product = require("../models/Product");
-const { getAllProducts } = require("../controllers/productController");
+const { getPaginatedProducts } = require("../controllers/productController");
 
-router.get("/", async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = 20;
-  const skip = (page - 1) * limit;
-
-  try {
-    const products = await Product.find().skip(skip).limit(limit).lean();
-    res.json(products);
-  } catch (err) {
-    console.error("Error fetching paginated products:", err);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+router.get("/", getPaginatedProducts);
 
 module.exports = router;
