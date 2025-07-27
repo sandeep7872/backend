8db31350d459b4f18c4bc0ca70c6fc494dde9650
sandeep7872 <93874397+sandeep7872@users.noreply.github.com>
2025-07-27 13:11:25 +0530

    Update products.js

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const { getPaginatedProducts } = require("../controllers/productController");
+const { 
+  getPaginatedProducts,
+  createProduct,
+  getProductById,
+  updateProduct,
+  deleteProduct
+} = require("../controllers/productController");
 
+// GET paginated products
 router.get("/", getPaginatedProducts);
+
+// GET single product
+router.get("/:id", getProductById);
+
+// POST create new product with multiple images
+router.post("/", createProduct);
+
+// PUT update product (including images)
+router.put("/:id", updateProduct);
+
+// DELETE product
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
